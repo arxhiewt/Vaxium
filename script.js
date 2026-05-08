e6a6db25b15d2cc195fe6c7f049cf6eeed5de3dd
arxhiewt <arxhie08@gmail.com>
2026-05-09 02:25:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Vaxium test loaded");
+const cards = document.querySelectorAll('.feature, .server, .bypass-card, .stat');
+
+cards.forEach(card => {
+
+  card.addEventListener('mouseenter', () => {
+    card.style.transform = 'translateY(-6px)';
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'translateY(0px)';
+  });
+
+});
+
+console.log(`
+=====================================
+        VAXIUM CLIENT
+=====================================
+Version: v1.1.0 ALPHA
+Minecraft: 1.19 → 1.21.11
+AI-Battler: ENABLED
+=====================================
+`);
