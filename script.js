500b67d531b4639f2f0b5a677282a94b0afbe13f
arxhiewt <arxhie08@gmail.com>
2026-05-09 14:54:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function openImg(img) {
-  document.getElementById("viewer").style.display = "flex";
-  document.getElementById("viewerImg").src = img.src;
-}
 
-function closeImg() {
-  document.getElementById("viewer").style.display = "none";
-}
